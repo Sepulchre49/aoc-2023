79d8a1d458e141ccfc12be567518a5c300a8af76
Caleb Fringer <cfringer98@gmail.com>
2023-12-23 18:48:47 -0800

    Renamed set->interval, implemented difference, union, intersection.

--- day5/set.lua
+++ day5/set.lua
@@ -1,33 +1,39 @@
-local Set = {}
+local Interval = {}
 
-function Set:new(lower, upper)
+function Interval:new(lower, upper)
     local o = {
 	["lower"] = lower or nil,
 	["upper"] = upper or nil,
     }
 
     self.__index = self
+    self.__mul = Interval.intersection
     setmetatable(o, self)
     return o
 end
 
-function Set:isEmpty()
+function Interval:isEmpty()
     return not (self.lower and self.upper)
 end
 
-function Set:size()
+function Interval:size()
     if self:isEmpty() then return nil end
     return self.upper - self.lower
 end
 
-function Set:intersection(other)
+function Interval:intersection(other)
     if self.upper < other.lower or other.upper < self.lower then 
-	return Set:new()
+	return Interval:new()
     end
 
-    local lower = self.lower < other.lower and self.lower or other.lower
+    local lower = self.lower > other.lower and self.lower or other.lower
     local upper = self.upper < other.upper and self.upper or other.upper
     return self:new(lower, upper)
 end
 
-return Set
+local A = Interval:new(1, 10)
+local B = Interval:new(5, 20)
+local C = A * B
+print(C.lower, C.upper)
+
+return Interval
