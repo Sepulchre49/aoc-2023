75a21f4767a499ddea4c95537d5a872ed6ead23c
Caleb Fringer <cfringer98@gmail.com>
2024-01-12 20:47:27 -0800

    Finished pt 2.

--- day8/solution.py
+++ day8/solution.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import re
+import math
 
 instructions = []
 graph = {}
@@ -10,6 +11,7 @@ with open("input.txt") as input:
 
         instruction = re.search(r"^[LR]+$", line)
         edge = re.match(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)", line)
+        #edge = re.match(r"(\w{3}) = \((\w{3}), (\w{3})\)", line)
 
         if instruction:
             instructions = list(instruction[0])
@@ -26,4 +28,19 @@ while current != "ZZZ":
     count = count + 1
     i = (i+1) % len(instructions)
 
-print(count)
+# Part two
+vertices = list(filter(lambda x: x[2] == "A", graph.keys()))
+counts = { vertex : 0 for vertex in vertices }
+
+for vertex in vertices:
+    i = 0
+    count = 0
+    current = vertex
+    while current[2] != "Z":
+        direction = instructions[i]
+        current = graph[current][0] if direction == "L" else graph[current][1]
+        i = (i+1) % len(instructions)
+        count = count + 1
+    counts[vertex] = count
+
+print(math.lcm(*counts.values()))
