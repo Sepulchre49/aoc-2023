ac7a4f589e464a890af9770e7b2556395cb1e3a5
CalebF98 <cfringer98@gmail.com>
2024-01-13 18:15:02 -0800

    Finished pt 2.

--- day9/solution.py
+++ day9/solution.py
@@ -41,9 +41,38 @@ def extrapolate(sequence):
 
     return tree[0][-1]
 
+def extrapolate2(sequence):
+    tree = [ sequence ] 
+
+    current = sequence
+    while not sequenceIsZero(current):
+        next = []
+        i = 1
+        while i < len(current):
+            next.append(current[i] - current[i-1])
+            i += 1
+        current = next
+        tree.append(current)
+
+    tree[-1].append(0)
+
+    i = len(tree) - 1
+    while i > 0:
+        tree[i-1].insert(0, tree[i-1][0] - tree[i][0])
+        i -= 1
+
+    return tree[0][0]
+
 sum = 0
 with PPE() as executor:
     results = executor.map(extrapolate, data)
+    sum = reduce(lambda x,y: x+y, results)
+
+print(sum)
+
+sum = 0
+with PPE() as executor:
+    results = executor.map(extrapolate2, data)
     sum = reduce(lambda x,y: x+y, results)
 
 print(sum)
